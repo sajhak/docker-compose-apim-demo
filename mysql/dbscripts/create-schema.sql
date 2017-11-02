114e00884c176f8f52e777dacb5b1f9b5486bbe4
Sajith Kariyawasam <sajhak@gmail.com>
2017-11-02 10:50:15 +0530

    Initia files for docker compose

---
+++ mysql/dbscripts/create-schema.sql
@@ -0,0 +1,6 @@
+-- Adding sample values --
+CREATE DATABASE IF NOT EXISTS ABC_RESTAURANT;
+USE ABC_RESTAURANT;
+CREATE TABLE authors (id INT, name VARCHAR(20), email VARCHAR(20));
+INSERT INTO authors (id,name,email) VALUES(1,"Vivek","xuz@abc.com");
+
